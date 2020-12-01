a6ae0e8117a1dbcb5e56e941bb7680c93c315b6c
bahayg <bahayg@gmail.com>
2020-12-01 12:32:51 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    range1 = (1...limit).to_a
+    arr = []
+    range1.map do |item|
+        if item%3 == 0 || item%5 == 0 
+            arr.push(item)
+        end
+    end
+    arr
+end
+
+# def collect_multiples
+#     (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0 }
+# end
+
+def sum_multiples(limit)
+    collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit, :multiples
+
+    def initialize(limit)
+        @limit = limit
+        @multiples = collect_multiples
+    end
+
+    def collect_multiples
+        range1 = (1...limit).to_a
+        arr = []
+        range1.map do |item|
+            if item%3 == 0 || item%5 == 0 
+                arr.push(item)
+            end
+        end
+        arr
+    end
+    
+    def sum_multiples
+        multiples.inject(:+)
+    end
+end
